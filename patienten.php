0c6facb0ccb48a5dea3cfaa750d8cafdc67a45cf
liesbetvs <r0485110@student.thomasmore.be>
2018-06-02 10:25:19 +0200

    nav

---
+++ patienten.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mål</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="css/styletwee.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+
+<div class=navigation>
+<a href=""><div class="logo"></div></a>
+<nav class="navbar-fixed-left">
+    <ul class="nav navbar-nav">
+    <li><a href="dashboard.php">Dashboard</a></li>
+    <li><a href="patienten.php">Patiënten</a></li>
+    <li><a href="modules.php">Modules</a></li>
+    </ul>
+</nav>
+</div>
+
+
+<div class="container">
+</div>
+
+</body>
+</html>
