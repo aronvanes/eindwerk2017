84baf0fe64fc8a5a0e48e24dd5540aeea275cb5a
aronvanes <r0615687@student.thomasmore.be>
2018-06-06 12:05:42 +0200

    function select en create modules interactie

--- classes/Module.class.php
+++ classes/Module.class.php
@@ -3,6 +3,96 @@ include_once("Db.class.php");
 
 class Module {
 
+    private $naam;
+    private $beschrijving;
+    private $categorie;
+    private $view_level;
 
-    
+    /**
+     * @return mixed
+     */
+    public function getNaam()
+    {
+        return $this->naam;
+    }
+
+    /**
+     * @param mixed $naam
+     */
+    public function setNaam($naam)
+    {
+        $this->naam = $naam;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBeschrijving()
+    {
+        return $this->beschrijving;
+    }
+
+    /**
+     * @param mixed $Besvhrijving
+     */
+    public function setBeschrijving($beschrijving)
+    {
+        $this->beschrijving = $beschrijving;
+    }
+    /**
+     * @return mixed
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * @param mixed $categorie
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getViewLevel()
+    {
+        return $this->view_level;
+    }
+
+    /**
+     * @param mixed $view_level
+     */
+    public function setViewLevel($view_level)
+    {
+        $this->view_level = $view_level;
+    }
+
+
+
+    public function GetAllInteractieModules(){
+
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT * FROM tbl_module WHERE categorie = :categorie");
+        $statement->bindParam(':categorie', 1);
+        $result = $statement->execute();
+        return $result;
+    }
+
+    public function CreateInteractieModule(){
+
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("INSERT INTO tbl_module(naam, beschrijving, categorie, view_level) 
+        VALUES (:naam, :beschrijving, :categorie, :view_level)");
+        $statement->bindParam(':naam', $this->naam);
+        $statement->bindParam(':beschrijving', $this->beschrijving);
+        $statement->bindParam(':categorie', $this->categorie);
+        $statement->bindParam(':view_level', $this->view_level);
+        $result = $statement->execute();
+        return $result;
+    }
+
 }
