287a2350cd66c40355c588ff9448c9324edeb622
aronvanes <r0615687@student.thomasmore.be>
2018-06-06 11:40:50 +0200

    removed emailfunction

--- classes/User.class.php
+++ classes/User.class.php
@@ -5,7 +5,6 @@ class User {
     private $usernaam;
     private $voornaam;
     private $achternaam;
-    private $email;
     private $wachtwoord;
     private $rol;
     //private $userid;
@@ -74,24 +73,7 @@ class User {
 
     /**
      * Get the value of email
-     */ 
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set the value of email
-     *
-     * @return  self
-     */ 
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
+     */
     /**
      * Get the value of wachtwoord
      */ 
