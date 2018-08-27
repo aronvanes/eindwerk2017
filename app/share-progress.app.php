84b6173997b559faf60b69088e259426f96c699b
AlexHermans <alexhermans90@gmail.com>
2018-08-27 16:17:39 +0200

    Sharen van vooruitgang mogelijk
    wordt opgeslagen in de db in tbl_berichten

---
+++ app/share-progress.app.php
@@ -0,0 +1,38 @@
+<?php
+spl_autoload_register(function($class){
+    include_once(dirname(__DIR__)."/classes/" .  $class . ".class.php");
+});
+
+//Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+
+$decoded = json_decode($content, true);
+
+if(is_array($decoded)) {
+
+  $numberOfSuccesses = 0;
+
+  $berichtStart = 'Hey! '.$decoded['sender']['firstName'].' wilt zijn-haar vooruitgang in de module '.$decoded['module']['naam'].' met je delen. Hij-zij heeft reeds '.$decoded['completed_tasks'].' van de '.$decoded['total_tasks'].' taken behaald.';
+  $berichtMiddle = '';
+  if ($decoded['message']){
+    $berichtMiddle = ' Hij-zij wou ook het volgende zeggen: "'.$decoded['message'].'""';
+  }
+  $berichtEnd = ' Antwoord op dit bericht om hem-haar te feliciteren met zijn-haar inspanningen!';
+
+  foreach ($decoded['destination'] as $destination){
+    $bericht = new Bericht();
+    $bericht->setVerzenderId((int)$decoded['sender']['user_id']);
+    $bericht->setOntvangerId((int)$destination['id']);
+    $bericht->setBericht($berichtStart.$berichtMiddle.$berichtEnd);
+
+    if($bericht->sendMessage()){
+      $numberOfSuccesses += 1;
+    }
+  }
+
+  echo json_encode($numberOfSuccesses == count($decoded['destination']));
+
+} else {
+  // Send error back to user.
+  echo ('json_format');
+}
--- classes/Bericht.class.php
+++ classes/Bericht.class.php
@@ -10,32 +10,33 @@ class Bericht {
   private $gelezen;
 
   public function setVerzenderId($value){
-    $this->$verzender_id = $value
+    $this->verzender_id = $value;
   }
 
   public function setOntvangerId($value){
-    $this->$ontvanger_id = $value
+    $this->ontvanger_id = $value;
   }
 
   public function setBericht($value){
-    $this->$bericht = $value
+    $this->bericht = $value;
   }
 
   public function setDatumVerstuurd($value){
-    $this->$datum_verstuurd = $value
+    $this->datum_verstuurd = $value;
   }
 
   public function setGelezen($value){
-    $this->$gelezen = $value
+    $this->gelezen = $value;
   }
 
   public function sendMessage() {
-    $conn = Db::getInstance()
+    $conn = Db::getInstance();
 
-    $statement = $conn->prepare('INSERT INTO tbl_messages (verzender_id, ontvanger_id, bericht, datum_verstuurd, gelezen) VALUES (:verzender_id, :ontvanger_id, :bericht, CURRENT_TIMESTAMP(), 0)');
+    $statement = $conn->prepare('INSERT INTO tbl_berichten (verzender_id, ontvanger_id, bericht, datum_verstuurd, gelezen) VALUES (:verzender_id, :ontvanger_id, :bericht, CURRENT_TIMESTAMP(), 0)');
     $statement->bindParam(':verzender_id', $this->verzender_id);
     $statement->bindParam(':ontvanger_id', $this->ontvanger_id);
     $statement->bindParam(':bericht', $this->bericht);
 
     return $statement->execute();
   }
+}
